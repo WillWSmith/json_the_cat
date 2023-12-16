ea84a390b88027dffd0ecbdca5bbe7b42529f9c0
Will Smith <will.w.smith94@gmail.com>
2023-12-16 12:20:12 -0500

    Bug fixes and additional test case

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,23 +5,25 @@ const fetchBreedDescription = function(breedName, callback) {
 
   request(url, (error, response, body) => {
     if (error) {
-      // If there's an error, pass the error to the callback
       callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(new Error(`Failed to fetch breed: Status Code ${response.statusCode}`), null);
       return;
     }
 
     const data = JSON.parse(body);
 
-    // Check if the response array is empty
     if (data.length === 0) {
-      // If breed not found, pass a message to the callback
-      callback(null, "Sorry, breed not found.");
+      callback(new Error("Breed not found"), null);
     } else {
-      // If successful, pass the description to the callback
       callback(null, data[0].description);
     }
   });
 };
+
 
 module.exports = {
   fetchBreedDescription,
---
+++ test/breedFetcherTest.js
@@ -0,0 +1,30 @@
+const { fetchBreedDescription } = require('../breedFetcher');
+const { assert } = require('chai');
+
+describe('fetchBreedDescription', () => {
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      // We expect no error for this scenario
+      assert.equal(err, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+
+      // Compare the returned description
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+  });
+
+  it('returns an error and null description for an invalid/non-existent breed, via callback', (done) => {
+    fetchBreedDescription('InvalidBreed', (err, desc) => {
+      // We expect an error for this scenario
+      assert.notEqual(err, null);
+
+      // Description should be null
+      assert.equal(desc, null);
+
+      done();
+    });
+  });
+});
